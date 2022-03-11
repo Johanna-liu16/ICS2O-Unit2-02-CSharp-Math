29d7e396d3a8d5fab140cf529c51bfe41f3a569d
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-11 17:53:55 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+// Created by: Johanna Liu
+// Created on: Mar 2022
+//
+// This program does basic math
+
+using System;
+
+class Program 
+{
+  
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine ("Hello World");
+  }
+}
