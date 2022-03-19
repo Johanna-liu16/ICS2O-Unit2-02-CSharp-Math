2148619d6be7409a7fdbf22c015406e8da28baa7
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-19 20:55:13 +0000

    changed code

--- main.cs
+++ main.cs
@@ -10,9 +10,8 @@ class Program
   public static void Main (string[] args) 
   {
     Console.WriteLine("The dimensions of a rectangle is 2 cm and 5 cm.");
-    Console.WriteLine("The area is: " + (2 * 5) + " cm²");
-    Console.WriteLine("The perimeter is: " + (2 * (2 + 5)) + " cm");
-
+    Console.WriteLine("The area is: "+ (2 * 5) + " cm²");
+    Console.WriteLine("The perimeter is: "+ (2 * (2 + 5)) + " cm");
     Console.WriteLine("\nDone.");
   }
 }
